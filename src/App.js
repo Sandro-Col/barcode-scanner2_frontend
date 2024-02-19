61f2cc1ed1407b310cf6c03b15c35b6f80311d1a
Sandro-Col <sandro.col@gmail.com>
2024-02-18 19:39:53 -0800

    First

---
+++ src/App.js
@@ -0,0 +1,141 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from "axios";
+
+function App() {
+    const [listOfProducts, setListOfProducts] = useState([]);
+    const [barcode, setBarcode] = useState("");
+    const [aisle, setAisle] = useState(0);
+    const [side, setSide] = useState("");
+    const [section, setSection] = useState(0);
+    const [shelf, setShelf] = useState(0);
+    const [shelfOrder, setShelfOrder] = useState(0);
+
+    // const Connection = "http://localhost:3001/";
+    const connection = "https://barcodescanner2-backend.onrender.com";
+
+
+    useEffect(() => {
+        Axios.get(connection + "getProducts").then((response) => {
+            setListOfProducts(response.data);
+        })
+    }, []);
+
+    const createProduct = () => {
+        Axios.post(connection + "createProduct", {
+            barcode: barcode,
+            aisle: aisle,
+            side: side,
+            section: section,
+            shelf: shelf,
+            shelfOrder: shelfOrder,
+        }).then((response) => {
+            setListOfProducts([
+                {
+                    barcode: barcode,
+                    aisle: aisle,
+                    side: side,
+                    section: section,
+                    shelf: shelf,
+                    shelfOrder: shelfOrder,
+                },
+                ...listOfProducts,
+            ]);
+        // alert("PRODUCT CREATED");
+        });
+    };
+
+    return (
+    <div className="App">
+
+        <input type="text" placeholder='UPC...' onChange={(event) => {setBarcode(event.target.value.trim());}}></input>
+        <input type="number" placeholder='Aisle...' onChange={(event) => {setAisle(event.target.value);}}></input>
+        <input type="text" placeholder='Side...' onChange={(event) => {setSide(event.target.value.trim().toUpperCase());}}></input>
+        <input type="number" placeholder='Section...' onChange={(event) => {setSection(event.target.value);}}></input>
+        <input type="number" placeholder='Shelf...' onChange={(event) => {setShelf(event.target.value);}}></input>
+        <input type="number" placeholder='Shelf Order...' onChange={(event) => {setShelfOrder(event.target.value);}}></input>
+
+        <button onClick={createProduct}> Create Product </button>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <div className='productDisplay'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Barcode</th>
+                        <th>Brand</th>
+                        <th>Name</th>
+                        <th>Aisle</th>
+                        <th>Side</th>
+                        <th>Section </th>
+                        <th>Shelf </th>
+                        <th>Shelf Position</th>
+                        <th>Category</th>
+
+                    </tr>   
+                </thead>
+                <tbody>
+                    {listOfProducts
+                        .map((product, index) => {
+                            const link = "https://go-upc.com/search?q=" + product.barcode
+                        return (
+                            <tr key={index}>
+                                <td>
+                                    <a href={link} className="link"> {product.barcode} </a>
+                                </td>
+                                <td>{product.brand}</td>
+                                <td>{product.name}</td>
+                                <td>{product.aisle}</td>
+                                <td>{product.side}</td>
+                                <td>{product.section}</td>
+                                <td>{product.shelf}</td>
+                                <td>{product.shelfOrder}</td>
+                                <td>{product.category}</td>
+
+                            </tr>          
+                        );
+                        })}
+                </tbody>
+            </table>
+        </div>
+    </div>
+    );
+}
+
+
+
+// function App() {
+
+// <div>
+//                     <h1>Barcode: {product.barcode}</h1>
+//                     <h2>Aisle: {product.aisle}</h2>
+//                     <h2>Side: {product.side}</h2>
+//                     <h2>Section: {product.section}</h2>
+//                     <h2>Shelf: {product.shelf}</h2>
+//                 </div>
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+
+export default App;
